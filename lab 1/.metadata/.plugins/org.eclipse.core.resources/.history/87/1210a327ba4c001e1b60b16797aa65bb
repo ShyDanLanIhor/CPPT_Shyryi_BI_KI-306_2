c065a59cd8ae4c7bc0474bbd08e92dd878cf1fed
ShyDanLanIhor <138773801+ShyDanLanIhor@users.noreply.github.com>
2023-09-11 02:03:47 +0300

    Зроблена лабораторна робота.

--- lab 1/.metadata/.plugins/org.eclipse.core.resources/.history/87/1210a327ba4c001e1b60b16797aa65bb
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"Lab2_Shyryi_CI_306","l":"Lab2_Shyryi_CI_306()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Lab2_Shyryi_CI_306","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
---
+++ lab 2/СPPT_labN2_Shyryi/src/CI_306/Shyryi/Lab2/models/Sailer.java
@@ -0,0 +1,101 @@
+package CI_306.Shyryi.Lab2.models;
+
+import CI_306.Shyryi.Lab2.FileUtil;
+
+/**
+ * The `Sailer` class represents a sailor who can row a boat. It extends the `Person` class.
+ */
+public class Sailer extends Person
+{
+	private Integer stamina;
+	private Integer maxStamina;
+	private Integer power;
+	private Integer experience;
+	
+    public Sailer() {
+        super();
+        System.out.print("\nПустий весляр був створений");
+        
+        FileUtil.appendToFile("Логи.txt", "Був створений " + this.toString());
+    }
+
+    public Sailer(String firstname, String lastname, Integer years, Integer stamina, 
+    		Integer power, Integer experience, Integer maxStamina) {
+        super(firstname, lastname, years);
+        this.stamina = stamina;
+        this.power = power;
+        this.experience = experience;
+        this.maxStamina = maxStamina;
+        
+        FileUtil.appendToFile("Логи.txt", "Був створений " + this.toString());
+    }
+
+    public Sailer(Sailer other) {
+        super(other);
+        this.stamina = other.stamina;
+        this.power = other.power;
+        this.experience = other.experience;
+        this.maxStamina = other.maxStamina;
+        
+        FileUtil.appendToFile("Логи.txt", "Був створений " + this.toString());
+    }
+
+    {
+        this.stamina = 0;
+        this.power = 0;
+        this.experience = 0;
+        this.maxStamina = 0;
+    }
+    
+    /**
+     * Restores the sailor's stamina to its maximum value.
+     */
+    public void Rest()
+    {
+    	this.stamina = this.maxStamina;
+    }
+
+    public Integer getStamina() {
+        return stamina;
+    }
+
+    public void setStamina(Integer stamina) {
+        this.stamina = stamina;
+    }
+
+    public Integer getPower() {
+        return power;
+    }
+
+    public void setPower(Integer power) {
+        this.power = power;
+    }
+
+    public Integer getExperience() {
+        return experience;
+    }
+
+    public void setExperience(Integer experience) {
+        this.experience = experience;
+    }
+
+    public String toHtml()
+    {
+    	return "<p>" + " ім'я = " + getFirstname() + ", прізвище = " + getLastname() + ",</p>" +
+        		"<p>" + "вік = " + getYears() +", енергія = " + stamina + ",</p>" +
+        		"<p>" + "потужність = " + power + ", досвід = " + experience + "</p>";
+    }
+    
+    @Override
+    public String toString() {
+        return "Весляр{" +
+                "ім'я='" + getFirstname() + '\'' +
+                ", прізвище='" + getLastname() + '\'' +
+                ", вік=" + getYears() +
+                ", енергія=" + stamina +
+                ", maxЕнергія=" + maxStamina +
+                ", потужність=" + power +
+                ", досвід=" + experience +
+                '}';
+    }
+}
